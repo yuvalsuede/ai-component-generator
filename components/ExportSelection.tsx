1f6f1604b332425f9c057ced63fa5d3bc294c18a
John zuo <johnzuo305@gmail.com>
2023-03-14 23:54:52 +1100

    [UI] fix when the text too long, it will cover the arrow in the select

--- components/ExportSelection.tsx
+++ components/ExportSelection.tsx
@@ -29,7 +29,7 @@ export default function ExportSelection({ onSelectedExport }) {
                 id="select-option"
                 value={selectedOption}
                 onChange={handleChange}
-                className="px-4 py-2 text-gray-700 bg-white border border-gray-400 rounded-md focus:outline-none focus:border-purple-500"
+                className="pl-4 pl-5 py-2 text-gray-700 bg-white border border-gray-400 rounded-md focus:outline-none focus:border-purple-500"
             >
                 {FRAMEWORKS.map((option) => (
                     <option key={option.value} value={option.value}>
